ebcee1c9d3c019b16b4da9d4bcdb05604fb7ae27
Roko Peruško <128609598+RokoPerusko@users.noreply.github.com>
2024-09-14 13:28:06 +0000

    Update settings.py

--- ZargonetkaProjekt/settings.py
+++ ZargonetkaProjekt/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d61sqhcg_6fi__rhk=h9!)_x+yx0jhxv=@*s0@__m14z8&0_fv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['zargonetka.org', 'www.zargonetka.org', '16.170.202.226']
 
 
 
@@ -167,6 +167,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
